92e62991d7202554c0928302f24e03f0b0ce176d
RobbeDenis <77392406+RobbeDenis@users.noreply.github.com>
2025-05-21 00:27:16 +0200

    Transferred test code to imgui project

---
+++ periodic_signal_detection/SignalDetectionWindow.cpp
@@ -0,0 +1,83 @@
+#include "SignalDetectionWindow.h"
+#include <numbers>
+
+void SDFunctions::PopulateBuffer(std::vector<float>& buffer, float samplerate, float period, bool printContents)
+{
+    using namespace std::numbers;
+
+    std::cout << "\n\nPopulating the buffer...\n";
+
+    const float amp{ 1.f };
+    const float freq{ period };
+
+    const float amp2{ 3.f };
+    const float freq2{ period * 2.f };
+
+    for (int i{ 0 }; i < buffer.size(); ++i)
+    {
+        float time{ (i / samplerate) * pi_v<float> };
+        float sample{ amp * std::sin(time * freq) };
+        float sample2{ amp2 * std::sin(time * freq2) };
+
+        buffer[i] = sample + sample2;
+
+        if (printContents)
+        {
+            std::cout << buffer[i] << ", ";
+        }
+    }
+
+    std::cout << "\nPopulating complete\n\n";
+}
+
+void SDFunctions::ClearBuffers(std::vector<float>& buffer, std::vector<float>& foldedBuffer)
+{
+    for (int i{ 0 }; i < buffer.size(); ++i)
+    {
+        buffer[i] = 0.f;
+        foldedBuffer[i] = 0.f;
+    }
+}
+
+bool SDFunctions::ShiftPositive(std::vector<float>& buffer, float& shift, bool printContents)
+{
+    const float min{ *std::min_element(begin(buffer), end(buffer)) };
+    const bool shifted{ min < 0.f };
+
+    std::cout << "\n\nChecking for buffer shift\n";
+    std::cout << "Min value in buffer: " << min << "\n";
+    if (shifted)
+    {
+        std::cout << "Started buffer shift...\n";
+        shift = std::abs(min);
+
+        for (float& sample : buffer)
+        {
+            sample += shift;
+
+            if (printContents)
+            {
+                std::cout << sample << ", ";
+            }
+        }
+    }
+
+    std::cout << "\nShifting complete\n\n";
+
+    return shifted;
+}
+
+void SDFunctions::FoldBuffer(std::vector<float>& buffer, std::vector<float>& foldedBuffer, float trailPeriod, bool printContents)
+{
+    std::cout << "\n\nFolding the buffer...\n";
+    for (int i{ 0 }; i < buffer.size(); ++i)
+    {
+        foldedBuffer[i] = buffer[i] - (static_cast<int>((buffer[i] / trailPeriod))) * trailPeriod;
+
+        if (printContents)
+        {
+            std::cout << foldedBuffer[i] << ", ";
+        }
+    }
+    std::cout << "\nFolding complete\n\n";
+}
---
+++ periodic_signal_detection/SignalDetectionWindow.h
@@ -0,0 +1,103 @@
+#pragma once
+#include "imgui.h"
+#include "implot.h"
+#include "implot_internal.h"
+
+#include <cmath>
+#include <vector>
+#include <iostream>
+
+namespace SDWindow
+{
+    /* data declarations */
+    enum BufferState : unsigned char
+    {
+        BS_Empty,
+        BS_Populated,
+        BS_Positive,
+        BS_Folded,
+    };
+
+    /* init */
+    static int bufferSize{ 3000 };
+    static float samplerate{ 32.f };
+    static float period{ 1.f };
+    static float trailPeriod{ 1.f };
+
+    /* state */
+    static BufferState bufferState{ BS_Empty };
+    static float shift{ 0.f };
+
+    /* buffers */
+    static std::vector<float> source(bufferSize, 0.f);
+    static std::vector<float> folded(bufferSize, 0.f);
+}
+
+namespace SDFunctions
+{
+    /* function declarations */
+    void PopulateBuffer(std::vector<float>& buffer, float samplerate, float period, bool printContents = false);
+    void ClearBuffers(std::vector<float>& buffer, std::vector<float>& foldedBuffer);
+    bool ShiftPositive(std::vector<float>& buffer, float& shift, bool printContents = false);
+    void FoldBuffer(std::vector<float>& buffer, std::vector<float>& foldedBuffer, float trailPeriod, bool printContents = false);
+}
+
+static void ShowSignalDetectionWindow()
+{
+    using namespace SDWindow;
+    using namespace SDFunctions;
+
+    bool disable{ false };
+
+    ImGui::Begin("Periodic signal detection");
+
+    /* Processing buttons */
+    disable = BS_Empty > bufferState;
+    if(disable) ImGui::BeginDisabled();
+    if (ImGui::Button("Populate buffer"))
+    {
+        PopulateBuffer(source, samplerate, period);
+        bufferState = BS_Populated;
+    }
+    ImGui::SameLine();
+    if (ImGui::Button("Clear buffers"))
+    {
+        ClearBuffers(source, folded);
+        bufferState = BS_Empty;
+    }
+    if(disable) ImGui::EndDisabled();
+
+    disable = BS_Populated > bufferState;
+    if (disable) ImGui::BeginDisabled();
+    if (ImGui::Button("Shift+"))
+    {
+        if (ShiftPositive(source, shift))
+        {
+            bufferState = BS_Positive;
+        }
+    }
+    if (disable) ImGui::EndDisabled();
+
+    ImGui::SameLine();
+
+    disable = BS_Positive > bufferState;
+    if (disable) ImGui::BeginDisabled();
+    if (ImGui::Button("Fold"))
+    {
+        FoldBuffer(source, folded, trailPeriod);
+        bufferState = BS_Folded;
+    }
+    if (disable) ImGui::EndDisabled();
+
+    /* Plots */
+    if (ImPlot::BeginPlot("Buffer")) 
+    {
+        ImPlot::PlotLine("Source", source.data(), bufferSize);
+        ImPlot::PlotLine("Folded", folded.data(), bufferSize);
+
+        ImPlot::EndPlot();
+    }
+
+    ImGui::End();
+}
+
